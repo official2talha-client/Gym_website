729235cbad7b6179ffbbcfa369f02f7e010c281e
Talha-Rahman-18 <talharahman055@gmail.com>
2026-09-14 04:47:54 +0600

    fix: Update CORS configuration to correct localhost port from 5173 to 5174

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ const app = express();
 
 app.use(cors({
      origin: [
-      "http://localhost:5173",
+      "http://localhost:5174",
       "https://gym-client-bay.vercel.app",
     ],
     credentials: true,
